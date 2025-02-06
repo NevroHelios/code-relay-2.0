cf4baafad6d0f67350e76175f1bd5bae23391f55
sayan chatterjee <chatterjeesayan1009@gmail.com>
2025-02-06 17:03:38 +0530

    [INIT]/add scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./components/header/Index";
+import Landing from "./components/Landing";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Landing/>
     </>
   );
 }
